d7fb53ec433a00dfa8cbd67018a7127acf2a2623
Joe Snell <joe.snell@daveramsey.com>
2018-09-04 12:59:04 -0500

    add new data

--- src/index.js
+++ src/index.js
@@ -1,33 +1,16 @@
 const { router, get } = require('microrouter')
 const data = require('./data.json')
-const ranges = require('./ranges.json')
-
-const stringify = () => {
-
-}
 
 const grade = (req, res) => {
 	const { grade, system } = req.query
 
 	const start = Date.now()
-
-	const isRange = grade && system && system in ranges && grade in ranges[system]
-
 	let result = grade && system ? data[system][grade] : {}
-
-	// if (isRange) {
-	// 	const range = ranges[system][grade]
-	// 	result = data[system][range]
-	// } else {
-		
-	// }
-
 	const end = Date.now()
 
 	const spacing = 'pretty' in req.query ? 2 : 0
 
 	return JSON.stringify({
-		isRange,
 		result,
 		meta: {
 			request_start: start,
@@ -41,7 +24,7 @@ const grade = (req, res) => {
 }
 
 const all = (req, res) => {
-	return JSON.stringify({ all: '' })
+	return JSON.stringify({ result: data })
 }
 
 const system = (req, res) => {
